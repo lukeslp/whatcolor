d52e986701a5d5b7dd9630bfe6a1c102ef2f301d
Luke Steuber <luke@lukesteuber.com>
2026-04-03 11:18:58 -0500

    session checkpoint: 2026-04-03 11:18

---
+++ android/app/src/main/java/one/whatcoloristhis/twa/WhatColorActivity.java
@@ -0,0 +1,10 @@
+package one.whatcoloristhis.twa;
+
+import android.os.Bundle;
+
+public class WhatColorActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
